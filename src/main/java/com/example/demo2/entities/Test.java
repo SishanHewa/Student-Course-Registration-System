38dcbf9f762d9cabf43f24faf9351d2791bf2bf0
Vimukthi Siriwardana <vimukthi.induwara2004@gmail.com>
2025-05-11 12:16:09 +0530

    Test Commit

--- src/main/java/com/example/demo2/entities/Test.java
+++ src/main/java/com/example/demo2/entities/Test.java
@@ -1,4 +1,27 @@
 package com.example.demo2.entities;
 
 public class Test {
+    private String courseId;
+    private String courseName;
+    private int courseDuration;
+    private double coursePrice;
+    private String lecturerName;
+    private String dayType;
+
+    public Test(String courseId, String courseName, int courseDuration, double coursePrice,
+                  String lecturerName, String dayType) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.courseDuration = courseDuration;
+        this.coursePrice = coursePrice;
+        this.lecturerName = lecturerName;
+        this.dayType = dayType;
+    }
+
+    public String getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public int getCourseDuration() { return courseDuration; }
+    public double getCoursePrice() { return coursePrice; }
+    public String getLecturerName() { return lecturerName; }
+    public String getDayType() { return dayType; }
 }
